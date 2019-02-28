9c225b515be8fde6bf1fd2a12b553a5af92da71e
Ajit Pawa <47364650+ajitpawa@users.noreply.github.com>
2019-02-28 12:55:48 -0700

    Add files via upload
    abstract class made for later implementation!

---
+++ Login.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public abstract class Login {
+
+  //ArrayList<User> userArray = New ArrayList<User>();
+
+  public abstract void signUp();
+
+  public abstract boolean logIn(String username, String password);
+
+  public abstract boolean logOut();
+
+}
