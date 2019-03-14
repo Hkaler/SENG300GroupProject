2c401b3466445a995257080d125e32053e69dfec
Jose Orellana <38052329+jose98orellana@users.noreply.github.com>
2019-03-14 13:28:42 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,8 +1,17 @@
-public class Driver {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Driver implements ActionListener{
 
 	public static boolean loginFlag = false; 
 	
 	public static void main(String[] args) {
 		GUI_Login.main(args);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
