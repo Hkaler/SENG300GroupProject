a1263185bc45b495b413397aa2aab7a5de947e9a
Jose Orellana <38052329+jose98orellana@users.noreply.github.com>
2019-03-14 13:24:07 -0600

    k

--- Driver.java
+++ Driver.java
@@ -1,3 +1,8 @@
-public static void main(String[] args) {
+public class Driver {
+
+	public static boolean loginFlag = false; 
 	
+	public static void main(String[] args) {
+		GUI_Login.main(args);
+	}
 }
